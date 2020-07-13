b1e36c4ae2e36303e5a0a29a433b5cf95662145d
kevduong <duong.kev@gmail.com>
2020-07-13 15:04:15 -0700

    Loaded data from API

--- app/src/main/java/com/example/pokedex/PokedexAdapter.java
+++ app/src/main/java/com/example/pokedex/PokedexAdapter.java
@@ -1,5 +1,7 @@
 package com.example.pokedex;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -10,6 +12,19 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,8 +43,7 @@ public class PokedexAdapter extends RecyclerView.Adapter<PokedexAdapter.PokedexV
         public void onClick(View view) {
           Pokemon current = (Pokemon) containerView.getTag();
           Intent intent = new Intent(view.getContext(), PokemonActivity.class);
-          intent.putExtra("name", current.getName());
-          intent.putExtra("number", current.getNumber());
+          intent.putExtra("url", current.getUrl());
 
           view.getContext().startActivity(intent);
         }
@@ -37,11 +51,43 @@ public class PokedexAdapter extends RecyclerView.Adapter<PokedexAdapter.PokedexV
     }
   }
 
-  private List<Pokemon> pokemon = Arrays.asList(
-          new Pokemon("Bulbasaur", 1),
-          new Pokemon("Ivysaur", 2),
-          new Pokemon("Venusaur", 3)
-  );
+  private List<Pokemon> pokemon = new ArrayList<>();
+  private RequestQueue requestQueue;
+
+  PokedexAdapter(Context context) {
+    requestQueue = Volley.newRequestQueue(context);
+    loadPokemon();
+  }
+
+  public void loadPokemon() {
+    String url = "https://pokeapi.co/api/v2/pokemon?limit=251";
+    JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+      @Override
+      public void onResponse(JSONObject response) {
+        try {
+          JSONArray results = response.getJSONArray("results");
+          for (int i = 0; i < results.length(); i++) {
+            JSONObject result = results.getJSONObject(i);
+            String name = result.getString("name");
+            pokemon.add(new Pokemon(
+                    name.substring(0, 1).toUpperCase() + name.substring(1),
+                    result.getString("url")
+            ));
+          }
+            notifyDataSetChanged();
+        } catch (JSONException e) {
+          Log.e("pokemon", "Json error", e);
+        }
+      }
+    }, new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+        Log.e("pokemon", "Pokemon list error");
+      }
+    });
+
+    requestQueue.add(request);
+  }
 
   @NonNull
   @Override
